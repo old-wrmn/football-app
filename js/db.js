01390f72ec6d94c884f7da52c2690267ebfd6c36
warmoa <moawarman@gmail.com>
2020-06-29 12:33:00 +0700

    db js fixed

--- js/db.js
+++ js/db.js
@@ -1,36 +1,41 @@
-let dbPromised = idb.open("football-app", 1, function(upgradeDb) {
-    let matchesObjectStore = upgradeDb.createObjectStore("matches", {
-      keyPath: id
-    });
-    matchesObjectStore.createIndex("utcDate", "utcDate", { unique: false });
+let dbPromised = idb.open("football-app", 1, function (upgradeDb) {
+  let matchesObjectStore = upgradeDb.createObjectStore("matches", {
+    keyPath: "id"
+  });
+  matchesObjectStore.createIndex("utcDate", "utcDate", {
+    unique: false
+  });
 });
 
 function saveForLater(data) {
-    dbPromised
-      .then(function(db) {
-        let tx = db.transaction("matches", "readwrite");
-        let store = tx.objectStore("matches");
-        console.log(data);
-        store.add({id: data.match.id, value: data.match});
-        //console.log(data.match);
-        //console.log(data.match.id);
-        return tx.complete;
-      })
-      .then(function() {
-        console.log("Match schedule has been saved.");
+  dbPromised
+    .then(function (db) {
+      let tx = db.transaction("matches", "readwrite");
+      let store = tx.objectStore("matches");
+      console.log(data);
+      store.add({
+        id: data.match.id,
+        value: data.match
       });
+      //console.log(data.match);
+      //console.log(data.match.id);
+      return tx.complete;
+    })
+    .then(function () {
+      console.log("Match schedule has been saved.");
+    });
 }
 
 function getAll() {
-    return new Promise(function(resolve, reject) {
-      dbPromised
-        .then(function(db) {
-          let tx = db.transaction("matches", "readonly");
-          let store = tx.objectStore("matches");
-          return store.getAll();
-        })
-        .then(function(matches) {
-          resolve(matches);
-        });
-    });
+  return new Promise(function (resolve, reject) {
+    dbPromised
+      .then(function (db) {
+        let tx = db.transaction("matches", "readonly");
+        let store = tx.objectStore("matches");
+        return store.getAll();
+      })
+      .then(function (matches) {
+        resolve(matches);
+      });
+  });
 }
